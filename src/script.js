903aa4fa21626fbc8831bac7e2490b8dfc877c8d
KilpikonnanKerma <nicorajala08@gmail.com>
2024-11-16 18:46:23 +0200

    about me

---
+++ src/script.js
@@ -0,0 +1,53 @@
+var keys = {32:1, 33: 1, 34:1, 35:1, 36: 1, 37: 1, 38: 1, 39: 1, 40: 1};
+
+function Start() {
+    disableScroll();
+}
+
+function preventDefault(e) {
+    e.preventDefault();
+}
+  
+function preventDefaultForScrollKeys(e) {
+    if (keys[e.keyCode]) {
+        preventDefault(e);
+        return false;
+    }
+}
+
+var supportsPassive = false;
+try {
+    window.addEventListener("test", null, Object.defineProperty({}, 'passive', { 
+        get: function () { supportsPassive = true; } 
+    }));
+} catch(e) {}
+
+var wheelOpt = supportsPassive ? { passive: false } : false;
+var wheelEvent = 'onwheel' in document.createElement('div') ? 'wheel' : 'mousewheel';
+
+function disableScroll() {
+    window.addEventListener('DOMMouseScroll', preventDefault, false); // older FF
+    window.addEventListener(wheelEvent, preventDefault, wheelOpt); // modern desktop
+    window.addEventListener('touchmove', preventDefault, wheelOpt); // mobile
+    window.addEventListener('keydown', preventDefaultForScrollKeys, false);
+}
+
+function showProjects() {
+    document.getElementById('projects').scrollIntoView({
+        block: 'start', behavior: 'smooth'});
+}
+
+function showAbout() {
+    document.getElementById('about').scrollIntoView({
+        block: 'start', behavior: 'smooth'});
+}
+
+function showSkills() {
+    document.getElementById('skills').scrollIntoView({
+        block: 'start', behavior: 'smooth'});
+}
+
+function backToTop() {
+    document.getElementById('header').scrollIntoView({
+        block: 'start', behavior: 'smooth'})
+}
