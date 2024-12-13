b97a4a7c4c9433f812522d6f577d74af645da23d
KilpikonnanKerma <nicorajala08@gmail.com>
2024-12-13 14:00:42 +0200

    almost light mode

--- src/script.js
+++ src/script.js
@@ -2,7 +2,15 @@ function Start() {
     //document.getElementById("navbar").style.top = "-100px";
 }
 
-window.onload = function() {document.getElementById("navbar").style.top = "-100px"; }
+window.onload = function() {
+    document.getElementById("navbar").style.top = "-100px"; ''
+    const theme = localStorage.getItem('theme');
+    if (theme) {
+        document.body.setAttribute('data-theme', theme);
+        const modeToggle = document.getElementById('mode-toggle');
+        modeToggle.textContent = theme === 'light' ? 'Dark Mode' : 'Light Mode';
+    }
+}
 
 var prevScrollpos = window.scrollY;
 window.onscroll = function() {
@@ -28,6 +36,21 @@ function show(item, position) {
     document.getElementById(item).scrollIntoView({
         block: position, behavior: 'smooth'
     });
+}
+
+function toggleMode() {
+    const body = document.body;
+    const modeToggle = document.getElementById('mode-toggle');
+
+    if (body.getAttribute('data-theme') === 'light') {
+        body.setAttribute('data-theme', 'dark');
+        modeToggle.textContent = 'Light Mode';
+        localStorage.setItem('theme', 'dark');
+    } else {
+        body.setAttribute('data-theme', 'light');
+        modeToggle.textContent = 'Dark Mode';
+        localStorage.setItem('theme', 'light');
+    }
 }
 
 function backToTop() {
